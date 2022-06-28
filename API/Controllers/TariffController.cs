355d0e1c4bddd1446beeda473eeb9769e6c3782d
alami <alami@km.ru>
2022-06-28 20:06:08 +0500

    dotnet ef database update

---
+++ API/Controllers/TariffController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TariffController : ControllerBase
+    {
+        [HttpGet] 
+        public string GetTariffs ()
+        {
+            return "List of tariffs";
+        }
+        [HttpGet("{id}")]
+        public string GetTariff(int id)
+        {
+            return "single tariffs";
+        }
+    }
+}
